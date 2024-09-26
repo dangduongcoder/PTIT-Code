590fe30c139ab7bfd034db60fd3d6f66fc8e4067
Truong Dang Duong <truongdangduong666@gmail.com>
2024-09-27 06:21:42 +0700

    upload code

---
+++ code.ptit/C02011.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+
+int main () {
+
+
+    int a, b;
+
+    scanf("%d %d", &a , &b);
+
+
+    for (int i = 1 ; i <= a ; i ++) {
+        if (i <= b) {
+            for (int j = i ; j <= b ; j ++) {
+                printf("%d", j);
+            }
+
+            for (int j = b - 1 ; j > b - i ; j --) {
+                printf("%d", j);
+            }
+
+        } else {
+            for (int j = i ; j > i - b ; j --) {
+                printf("%d", j);
+            }
+        }
+
+
+        printf("\n");
+    }
+
+
+
+    return 0;
+}
