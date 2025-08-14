5330fae8bd9cbfc21648b728be8b1fd996c004e2
Truong Dang Duong <truongdangduong666@gmail.com>
2025-08-14 15:11:53 +0700

    fix: load face detect model

---
+++ dsa/CTDL_005.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr)
+#define ll long long
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define sz(x) (int)(x).size()
+#define rep(i,a,b) for(int i=a; i<b; ++i)
+#define rrep(i,a,b) for(int i=a; i>=b; --i)
+
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9+7;
+const int N = 1e9+7;
+
+void solve() {
+
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for ( int &i: arr) cin >> i;
+
+    int k;
+    cin >> k;
+
+
+    for (int i : arr) {
+        if (i != k) cout << i << " ";
+    }
+
+
+
+}
+
+int main() {
+    fast_io;
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+    return 0;
+}
